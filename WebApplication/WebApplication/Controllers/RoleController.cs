cccfd1d1caf21d46cb04ceb2c4b1ab3296953cca
DESKTOP-88G7FUP\jinch <quantm@caerux.com>
2020-10-06 11:24:42 +0700

    no message

---
+++ WebApplication/WebApplication/Controllers/RoleController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext context;
+
+        public RoleController() 
+        {
+            context = new ApplicationDbContext();
+        }
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            var roles = context.Roles.ToList();
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            var role = new IdentityRole();
+            return View(role);
+        }
+
+        [HttpPost]
+        public ActionResult Create(IdentityRole role)
+        {
+            context.Roles.Add(role);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication/WebApplication/CustomFilter/AuthLogAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.CustomFilter
+{
+    public class AuthLogAttribute : AuthLogAttribute
+    {
+        public AuthLogAttribute()
+        {
+            View = "Authorization Failed";
+        }
+
+        public string View { get; set; }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+            IsUserAuthorized(filterContext);
+        }
+
+        private void IsUserAuthorized(AuthorizationContext filterContext)
+        {
+            if (filterContext.Result == null)
+                return;
+            if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                var vr = new ViewContext();
+                vr.ViewName = View;
+
+                ViewDataDictionary dict = new ViewDataDictionary();
+                dict.Add("Message", "Sorry you are not authorized to view this page");
+
+                vr.ViewData = dict;
+                var result = vr;
+                filterContext.Result = result;
+            }
+        }
+    }
+}
