a5758af63d7e8d7c78dbc6382125eba7e6b936dd
quantmth1908038 <54584382+quantmth1908038@users.noreply.github.com>
2020-10-09 09:28:45 +0700

    Lab_06

---
+++ ContosoCrafts/ContosoCrafts.WebSite/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductsService ProductsService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, 
+            JsonFileProductsService productsService)
+        {
+            _logger = logger;
+            ProductsService = productsService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductsService.GetProducts();
+        }
+    }
+}
